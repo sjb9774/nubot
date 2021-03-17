854986b0813e4dea6713067d9fd5446416c44d84
Stephen Biston <sjb9774@gmail.com>
2021-03-16 20:20:26 -0500

    Use app-module-path to correct dirs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@
       apps: [
         {
           name: 'nubot',
-          script: '/home/centos/prod/releases/20210315045109/bot.js',
+          script: '/home/centos/prod/releases/20210316154351/bot.js',
           watch: true,
           autorestart: true,
           restart_delay: 1000,
