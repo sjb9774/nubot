81072349fc9ce0514b3346a0ca21a1fe15e80605
Stephen Biston <sjb9774@gmail.com>
2021-03-18 08:15:08 -0500

    Use fuse fuzzy searching for bst

--- nubot/commands/bst.js
+++ nubot/commands/bst.js
@@ -1,4 +1,5 @@
 const bst = require('../../bst.json');
+const Fuse = require('fuse.js');
 
 function capitalize(str) {
   return str.replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())));
@@ -13,10 +14,14 @@ function execute(pokemon) {
         return "0 LUL";
     }
 
-    if (!bst[pokemon]) {
-      return `No stats found for "${pokemon}"`;
+    const availablePokes = Object.keys(bst);
+    const results = new Fuse(availablePokes, {includeScore: true}).search(pokemon);
+    if (results.length) {
+      console.log(`bst for ${pokemon} reported as ${results[0].item} with score ${results[0].score}`);
+      pokemon = results[0].item;
+      return `${capitalize(pokemon)} base stat total: ${bst[pokemon]}`;
     }
-    return `${capitalize(pokemon)} base stat total: ${bst[pokemon]}`;
+    return `No stats found for "${pokemon}"`;
 }
 
 module.exports = {
