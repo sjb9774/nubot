52f89acc77ae6ed831da8b4e90ef28112d32e7e6
Stephen Biston <sjb9774@gmail.com>
2021-07-31 15:42:34 -0500

    Update in accordance with stevebot updates

--- bot.js
+++ bot.js
@@ -2,9 +2,9 @@ const { Bot } = require('stevebot');
 const setup = require('./command-setup.js')
 
 const Nubot = new Bot({
-    commandsDir: `${process.env.NODE_PATH}/commands`,
+    commandsDir: `${process.env.NODE_PATH}/commands/`,
     globalPermission: setup.defaultPermissions,
-    commandPreSetupHook: setup.commandPreSetupHookd
+    commandPreSetupHook: setup.commandPreSetupHook
 });
 
 module.exports = {
