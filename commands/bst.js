ef8ea9add56ad03144d32b0e85afa4559ed7a382
Stephen Biston <sjb9774@gmail.com>
2022-01-26 10:08:13 -0600

    Dont say "undefined" please

--- commands/bst.js
+++ commands/bst.js
@@ -19,7 +19,9 @@ function execute(pokemon) {
     if (results.length) {
       console.log(`bst for ${pokemon} reported as ${results[0].item} with score ${results[0].score}`);
       pokemon = results[0].item;
-      return `${capitalize(pokemon)} base stat total: ${bst[pokemon]}`;
+      if (pokemon) {
+        return `${capitalize(pokemon)} base stat total: ${bst[pokemon]}`;
+      }
     }
     return `No stats found for "${pokemon}"`;
 }
