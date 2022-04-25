eb743f38468fc9ca34d054fe1e5ff790f3c6b144
Stephen Biston <sjb9774@gmail.com>
2022-04-25 00:02:30 -0500

    Differentiate env on homepage

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const cfg = require('./nubot/config.js')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 9000
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Welcome to Nubot!');
+  const message = `Welcome to Nubot (${cfg.getEnvConfig('APP_ENV')})!`
+  res.send(message);
 });
 app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`));
