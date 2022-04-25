b8170e0062cf0edec8f795686a21d62643dcee73
Stephen Biston <sjb9774@gmail.com>
2022-04-24 23:02:16 -0500

    Add procfile to separate bot and server

--- bot.js
+++ bot.js
@@ -67,13 +67,3 @@ client.connect().catch(console.error);
 function onConnectedHandler (addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
 }
-
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 9000
-
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Welcome to Nubot!');
-});
-app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`));
